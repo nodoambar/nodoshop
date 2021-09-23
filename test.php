a30ce61553377325aad678b42e0e96db6ec04e79
Quim Calpe <quim@kalpe.com>
2021-09-23 15:57:36 +0200

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-use NodoShop\Conector;
-
-require_once "vendor/autoload.php";
-
-$conector = new Conector("nodoambar", "5b694cd7-d8e3-493a-8d33-fed137755424");
-print_r($conector->productos());
